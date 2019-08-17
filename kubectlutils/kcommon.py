9c9174b009729d63b6bbc7c607b94efa58a047ce
Jordi Carrillo <jordilin@gmail.com>
2019-08-17 12:29:45 -0700

    kcommon: Add logs and events at the end of describe
    This allows us to quickly check logs for each container in the pod and
    events in the given namespace
    
    Signed-off-by: Jordi Carrillo <jordilin@gmail.com>

--- kubectlutils/kcommon.py
+++ kubectlutils/kcommon.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import subprocess
+
 
 def get_command(argvs, unit):
     cmd = []
@@ -55,17 +57,45 @@ def format(lines, namespace, describer='kdp'):
 
 
 def describe(argvs, describer):
-    import subprocess
+    logcmd = "kubectl logs "
+    eventscmd = "kubectl get events "
+    podname = ""
+    namespace = ""
     if len(argvs) == 3:
         # kdp pod namespace
         # pod name + namespace
         cmd = ["kubectl", "describe", describer, argvs[1], "-n", argvs[2]]
+        logcmd += argvs[1] + " -n " + argvs[2]
+        eventscmd += "-n " + argvs[2]
+        podname = argvs[1]
+        namespace = argvs[2]
     else:
         fields = aargvs[1].split(' ')
+        podname = fields[0]
         if len(fields) == 0:
             cmd = ["kubectl", "describe", describer, fields[0]]
+            logcmd += fields[0]
         else:
             cmd = [
                 "kubectl", "describe", describer, fields[0], "-n", fields[1]
             ]
+            logcmd += fields[0] + " -n " + fields[1]
+            eventscmd += "-n " + fields[1]
+            namespace = fields[1]
     print subprocess.check_output(cmd)
+    containers = list_containers_in_pod(podname, namespace)
+    if len(containers) == 0:
+        print "CHECK LOGS: (" + logcmd + ")"
+    else:
+        for c in containers:
+            print "CHECK LOGS: (" + logcmd + " -c " + c + ")"
+    print "CHECK EVENTS: (" + eventscmd + ")"
+    print
+
+
+def list_containers_in_pod(podname, namespace):
+    cmd = "kubectl get pod " + podname + " -n " + namespace + " -o jsonpath={.spec.containers[*].name}"
+    if namespace is None:
+        cmd = "kubectl get pod " + podname + " -o jsonpath={.spec.containers[*].name}"
+    containers = subprocess.check_output(cmd.split())
+    return containers.split()
